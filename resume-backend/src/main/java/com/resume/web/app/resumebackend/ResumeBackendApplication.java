c5c1e6fc01079363282335c18dd7191bbda8ac13
Shawn Fong <shawnxsf@gmail.com>
2020-06-25 20:23:55 -0400

    Base JPA Spring framework

---
+++ resume-backend/src/main/java/com/resume/web/app/resumebackend/ResumeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.resume.web.app.resumebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeBackendApplication.class, args);
+	}
+
+}
